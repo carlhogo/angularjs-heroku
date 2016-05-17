4c0b09de7ff498b3dfc6e2b9618419683d1bb21a
Carlos Hernán Hoyos Gómez <carlos.gomez88@gmail.com>
2016-05-16 21:05:46 -0500

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularjsHerokuApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularjsHerokuApp
+ */
+angular.module('angularjsHerokuApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
