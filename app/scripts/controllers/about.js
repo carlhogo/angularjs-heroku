9a73cfdfd6aa06299fdd262943952bea2e55e4f4
Carlos Hernán Hoyos Gómez <carlos.gomez88@gmail.com>
2016-06-09 18:20:34 -0500

    listo

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 /**
@@ -8,11 +9,15 @@
  * Controller of the angularjsHerokuApp
  */
 angular.module('angularjsHerokuApp')
-  .controller('AboutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('AboutCtrl', ['$http', function ($http) {
+    console.log('paso1');
+    var vm = this;
+    console.log('paso2');
+    $http.get('https://calm-fjord-47282.herokuapp.com/prueba/home/saludo').
+      success(function(data){
+      console.log('paso3');
+        vm.saludo = data;
+      console.log(data);
+    });
+  }]);
 
